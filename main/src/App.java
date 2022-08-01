561ced299606fe493c6febe69494485a08f08291
FernandoGarcia1 <fernandogarciadolores@gmail.com>
2022-08-01 12:17:46 -0500

    regreso main

--- main/src/App.java
+++ main/src/App.java
@@ -1,5 +1,11 @@
+import control.Control_Socios;
+import entities.Conexion;
+import entities.Socio;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
         System.out.println("Hello, World!");
+
     }
 }
