9e118333308427ad89cfe64942fa33e2d106e24d
Samuel <44588639+SamuelAc1011@users.noreply.github.com>
2022-08-04 17:56:39 -0500

    feat(Control): :sparkles: Mostrar estante
    Se agregó el comportamiento de la función mostrarEstante

--- main/src/control/Control_Estantes.java
+++ main/src/control/Control_Estantes.java
@@ -1,39 +1,32 @@
 package control;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 import entities.Estante;
 import entities.Seccion;
 
 /**
- * 
+ * Control Estantes.
+ *
+ * @author Samuel Alcántara Chávez
+ * @version 1.0
  */
 public class Control_Estantes {
 
     /**
-     * Default constructor
+     * Constructor vacío
      */
     public Control_Estantes() {
     }
 
-    /**
-     * 
-     */
+    // Atributos
     private Estante objEstante;
-
-    /**
-     * 
-     */
     private Seccion objSeccion;
-
-    /**
-     * 
-     */
     private List<Estante> listEstantes;
-
-    /**
-     * 
-     */
     private List<Seccion> listSecciones;
 
     /**
@@ -44,12 +37,39 @@ public class Control_Estantes {
     }
 
     /**
-     * @param id
-     * @return
+     * Método para obtener la información de un estante específico.
+     *
+     * @param id Identificador del estante
+     * @return Estante -> Información del estante.
      */
-    public Estante mostrarEstante(String id) {
-        // TODO implement here
-        return null;
+    public Estante mostrarEstante(String nombre) {
+        // Variables
+        Connection conexion = ConexionBD.connectDatabase();
+        PreparedStatement query = null;
+        ResultSet rs = null;
+
+        // Action: Realizar consulta con la base de datos
+        try {
+            query = conexion.prepareStatement(
+                    "SELECT e.idestante, e.nombre AS estante, s.nombre AS seccion FROM Estante e, Seccion s WHERE e.idseccion = s.idseccion AND e.nombre = ?;");
+            query.setString(1, nombre);
+            rs = query.executeQuery();
+            while (rs.next()) {
+                objEstante.setId((rs.getString("idestante")));
+                objEstante.setNombre((rs.getString("estante")));
+                objEstante.setSeccion((rs.getString("seccion")));
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conexion.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+
+        return objEstante;
     }
 
     /**
@@ -57,15 +77,6 @@ public class Control_Estantes {
      */
     public void crearSeccion(String nombre) {
         // TODO implement here
-    }
-
-    /**
-     * @param id
-     * @return
-     */
-    public Seccion mostrarSeccion(String id) {
-        // TODO implement here
-        return null;
     }
 
     /**
